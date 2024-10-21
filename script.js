128c8003ca570cb79921b3b301a5ea26e16abcd8
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 22:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getLevelCount(level) {
+  if (!level.parentNode) {
+    return 0;
+  }
+
+  level = level.parentNode;
+
+  return 1 + getLevelCount(level);
+}
+
+let level = document.getElementById("level");
+alert(`The level of the element is: ${getLevelCount(level)}`);
